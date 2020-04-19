33caa83a6a0ab8eb655cdd054ceb7e6bf3b4b5c0
Sergey Zolotarev <sryze@protonmail.com>
2020-04-19 20:23:34 +0600

    Implement compound selector parsing

--- css2sass.js
+++ css2sass.js
@@ -2,10 +2,6 @@
 
 const fs = require('fs');
 
-function parseCSSSelector(selector) {
-    return selector;
-}
-
 function parseCSSRules(css) {
     const RE_SELECTOR = /[\sa-zA-Z0-9\.\-+>*#:=()\[\]"']/;
     const RE_PROPERTY = /[a-zA-Z0-9-]/;
@@ -75,8 +71,8 @@ function parseCSSRules(css) {
 
     for (let i = 0; i < css.length; i++) {
         c = css[i];
-        console.log(i, state);
-        console.log('c: "' + c + '"`');
+        // console.log(i, state);
+        // console.log('c: "' + c + '"`');
         
         switch (state) {
             case ST_TOP:
@@ -192,6 +188,117 @@ function parseCSSRules(css) {
     return rules;
 }
 
+function parseCSSSelector(s) {
+    const RE_ALPHA = /\w/;
+    const RE_DELIM = /([.+~>:\[]|::)/;
+    const RE_ID = /[a-zA-Z0-9-]/;
+    const RE_WHITESPACE = /[\s\r\n]/;
+    
+    const ST_DELIM = 'DELIM';
+    const ST_ID = 'ID';
+    const ST_CLASS = 'CLASS';
+    const ST_PCLASS = 'PCLASS';
+    const ST_PCLASS_IN = 'PCLASS_IN';
+    const ST_TAG = 'TAG';
+    const ST_ATTR_BEGIN = 'ATTR_BEGIN';
+    
+    let c;
+    let state = ST_DELIM;
+    let name = '';
+    let selector = '';
+    const components = [];
+    
+    function error() {
+        throw new Error(`Unexpected character '${c}' in '${s}'`);
+    }
+    
+    function endComponent() {
+        if (!name) {
+            return;
+        }
+        
+        switch (state) {
+            case ST_ID:
+                components.push({name, selector, type: 'id'});
+                break;
+            case ST_CLASS:
+                components.push({name, selector, type: 'class'});
+                break;
+            case ST_PCLASS:
+                components.push({name, selector, type: 'pclass'});
+                break;
+            case ST_TAG:
+                components.push({name, selector, type: 'tag'});
+                break;
+        }
+        
+        name = selector = '';
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        c = s[i];
+        // console.log(c, state);
+        
+        switch (state) {
+            case ST_DELIM:
+                if (RE_WHITESPACE.test(c)) {
+                    if (c == ' ' || c == '\t') {
+                        selector += c;
+                    }
+                } else {
+                    if (c == '#') {
+                        state = ST_ID;
+                    } else if (c == '.') {
+                        state = ST_CLASS;
+                    } else if (c == ':') {
+                        state = ST_PCLASS;
+                    } else if (RE_ALPHA.test(c)) {
+                        state = ST_TAG;
+                        name += c;
+                    } else if (RE_DELIM.test(c)) {
+                        // ok?
+                    } else {
+                        error();
+                    }
+                    selector += c;
+                }
+                break;
+            case ST_ID:
+            case ST_CLASS:
+            case ST_PCLASS:
+            case ST_TAG:
+                if (RE_WHITESPACE.test(c)) {
+                    endComponent();
+                } else if (RE_DELIM.test(c)) {
+                    endComponent();
+                    i--;
+                    state = ST_DELIM;
+                } else if (RE_ID.test(c)) {
+                    name += c;
+                    selector += c;
+                } else if (c == '(' && state == ST_PCLASS) {
+                    state = ST_PCLASS_IN;
+                    name += c;
+                    selector += c;
+                } else {
+                    error();
+                }
+                break;
+            case ST_PCLASS_IN:
+                if (c == ')') {
+                    state = ST_PCLASS;
+                }
+                name += c;
+                selector += c;
+                break;
+        }
+    }
+    
+    endComponent();
+        
+    return components;
+}
+
 if (process.argv.length <= 2) {
     console.error('Usage: css2sass <css_file_path>');
     process.exit();
@@ -201,3 +308,13 @@ const css = fs.readFileSync(process.argv[2], 'utf-8');
 const rules = parseCSSRules(css);
 
 console.log(JSON.stringify(rules, null, 4));
+
+// console.log(parseCSSSelector('a'));
+// console.log(parseCSSSelector('#content'));
+// console.log(parseCSSSelector('.header'));
+// console.log(parseCSSSelector('.header.mobile'));
+// console.log(parseCSSSelector('.header > nav'));
+// console.log(parseCSSSelector('.header > nav > li'));
+// console.log(parseCSSSelector('.button:active'));
+// console.log(parseCSSSelector('.button:not(:active)'));
+// console.log(parseCSSSelector('.button:not(:active):not(:focus)'));
