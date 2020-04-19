77a6c90f24f012599fed8fdf593ef26ddce77e4a
Sergey Zolotarev <sryze@protonmail.com>
2020-04-20 02:03:43 +0600

    Add VSCode extension

---
+++ vscode/extension.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const vscode = require('vscode');
+const {parseRules, createRuleTree, printRuleTree} = require('../css2sass');
+
+function transformEditorSelection(transformer) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return; // No open text editor
+    }
+
+    editor.edit(editBuilder => {
+        editor.selections.forEach(selection => {
+            const text = editor.document.getText(new vscode.Range(selection.start, selection.end));
+            editBuilder.replace(selection, transformer(text));
+        });
+    });
+}
+
+function convertToSCSS(text) {
+	return printRuleTree(createRuleTree(parseRules(text)));
+}
+
+function activate(context) {
+    context.subscriptions.push(
+        vscode.commands.registerCommand('css2sass.convertToSCSS', () => {
+            transformEditorSelection(convertToSCSS);
+        })
+    );
+}
+
+module.exports = {activate};
